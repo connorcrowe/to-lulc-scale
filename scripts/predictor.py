8f310a15dbad0bb75c1c8eeb76378e24cedeef67
Connor Crowe <connor.haz.crowe@gmail.com>
2025-04-16 10:21:52 -0400

    Add auto-skipping if prediction file already exists. Switch to new model. Fix to saving CRS in metadata.

--- scripts/predictor.py
+++ scripts/predictor.py
@@ -15,15 +15,21 @@ import tensorflow.keras.backend as K
 
 from tensorflow.keras.models import load_model
 from rasterio.plot import reshape_as_image
+from rasterio.crs import CRS
 from patchify import patchify
 from patchify import unpatchify
+
+from pyproj import datadir
+
+# Use pyproj's known-good proj.db location
+os.environ["PROJ_DATA"] = str(datadir.get_data_dir())
 
 # Set directories
 DIR_IN_TILES = 'data/tiles/'
 DIR_OUT_PREDICTIONS = 'data/predictions/'
 
 # Set classification model
-PATH_MODEL = 'model/unet_model_512pd_4layer_batch_drop_5deepkernel.keras'
+PATH_MODEL = 'model/unet_v2_0.keras'
 
 # Function - Import tile
 def import_tiles():
@@ -44,46 +50,49 @@ def predict_tile(model, tile):
     Runs LULC model on a single tile
     """
     # Reshape from (bands, size, size) to (size, size, bands)  
-    tile_reshaped = reshape_as_image(tile)                                              #  (512, 512, 3)
+    tile_reshaped = reshape_as_image(tile)                                              #  (2048, 2048, 3)
+    tile_size = tile_reshaped.shape[0]
 
     # Split into patches of model input size to predict (128 x 128)
     patch_size = 128
-    patch_step = 128
-    patches = patchify(tile_reshaped, (patch_size, patch_size, 3), step = patch_step)   # (4, 4, 128, 128, 3)
+    patch_step = 64
+    patches = patchify(tile_reshaped, (patch_size, patch_size, 3), step = patch_step)   # (16, 16, 128, 128, 3)
 
     # Flatten to one list of patch_size x patch_size patches
-    patches_reshaped = patches.reshape(-1, patch_size, patch_size, 3)                   # (16, 128, 128, 3)
+    patches_reshaped = patches.reshape(-1, patch_size, patch_size, 3)                   # (256, 128, 128, 3)
     
     # Predict patches
-    prediction = model.predict(patches_reshaped)                                        # (16, 128, 128, 5)
-    
+    prediction = model.predict(patches_reshaped)                                        # (256, 128, 128, 5)
+
     # Unpatchify back to original shape
     prediction_reshaped = prediction.reshape(
         patches.shape[0], patches.shape[0], 1, patch_size, patch_size, 5)               # (4, 4, 1, 128, 128, 5)
-    prediction_unpatched = unpatchify(prediction_reshaped, (512, 512, 5))               # (512, 512, 5)
+    prediction_unpatched = unpatchify(prediction_reshaped, (tile_size, tile_size, 5))   # (2048, 2048, 5)
 
     # Collapse to highest likelihood class for each pixel
-    prediction_classes = np.argmax(prediction_unpatched, axis=-1)                       # (512, 512)
+    prediction_classes = np.argmax(prediction_unpatched, axis=-1)                       # (2048, 2048)
     
     return prediction_classes
 
 # Function - Save tile
-def save_tile(prediction, meta, tile_filename):
+def save_tile(prediction, meta, filepath):
     """
     Write prediction output to GeoTIFF
     """
-    # Set filepath
-    pred_filename = f'pred_{tile_filename}'
-    filepath = os.path.join(DIR_OUT_PREDICTIONS, pred_filename)
-
     # Update metadata (reduced band count from aerial)
     meta.update({
-        'count': 1,
-        'dtype': 'uint8'
+        "driver": "GTiff",
+        "height": 2048,
+        "width": 2048,
+        "count": 1,
+        "dtype": "uint8",
     })
+
+    meta["crs"] = CRS.from_epsg(3857)
 
     # Save prediction
     prediction = np.expand_dims(prediction, axis=0)
+    print(prediction.shape)
     with rasterio.open(filepath, "w", **meta) as dst:
         dst.write(prediction)
 
@@ -98,22 +107,23 @@ def main():
     tiles = import_tiles()
 
     # Lazy load tile, predict, save and garbage collect
-    stop_after = 100
-    skip = 60
+    stop_after = 1
     for tile_image, meta, filename in tiles:
-        if skip >= 0: 
-            skip -= 1
-            continue
         if stop_after <= 0: break
         stop_after -= 1
 
+        # Skip if already predicted
         print(f"Processing: {filename}")
+        out_path = os.path.join(DIR_OUT_PREDICTIONS, f'pred_{filename}')
+        if os.path.exists(out_path): 
+            print('skip')
+            continue
 
        # Predict
         prediction = predict_tile(model, tile_image)
 
         # Save
-        save_tile(prediction, meta, filename)
+        save_tile(prediction, meta, out_path)
     # Garbage Collect
     
 if __name__ == "__main__": main()
