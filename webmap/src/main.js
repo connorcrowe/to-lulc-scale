87668736055d7ce321b33094cb8a9404280073c0
Connor Crowe <connor.haz.crowe@gmail.com>
2025-04-25 15:45:09 -0400

    Add aerial layer, add legend, to webmap

--- webmap/src/main.js
+++ webmap/src/main.js
@@ -1,5 +1,6 @@
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
+import * as esri from "esri-leaflet";
 
 // Initialize the map
 const map = L.map("map", {
@@ -14,6 +15,12 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution: "© OpenStreetMap contributors",
 }).addTo(map);
 
+// Add aerial imagery from Toronto MapServer
+const aerialLayer = esri.tiledMapLayer({
+  url: "https://gis.toronto.ca/arcgis/rest/services/basemap/cot_ortho_2023_color_10cm/MapServer",
+  attribution: "City of Toronto Aerial Imagery 2023",
+}).addTo(map);
+
 // Add prediction tiles layer
 const predictionLayer = L.tileLayer("tiles_colour/{z}/{x}/{y}.png", {
   tms: true, 
@@ -21,14 +28,27 @@ const predictionLayer = L.tileLayer("tiles_colour/{z}/{x}/{y}.png", {
   opacity: 0.9,
 }).addTo(map);
 
-// Toggle logic
-const toggleButton = document.getElementById("toggle-prediction");
-toggleButton.addEventListener("click", () => {
-  if (map.hasLayer(predictionLayer)) {
+
+// Toggle Prediction Layer
+const predToggle = document.getElementById("toggle-prediction");
+let predVisible = true;
+predToggle.addEventListener("click", () => {
+  if (predVisible) {
     map.removeLayer(predictionLayer);
-    toggleButton.textContent = "Show Prediction";
   } else {
     map.addLayer(predictionLayer);
-    toggleButton.textContent = "Hide Prediction";
   }
+  predVisible = !predVisible;
+});
+
+// Toggle Aerial Layer
+const aerialToggle = document.getElementById("toggle-aerial");
+let aerialVisible = false;
+aerialToggle.addEventListener("click", () => {
+  if (aerialVisible) {
+    map.removeLayer(aerialLayer);
+  } else {
+    map.addLayer(aerialLayer);
+  }
+  aerialVisible = !aerialVisible;
 });
